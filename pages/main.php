8a51f640583bbfa109f019da47540ee4af244d87
unknown <bika070705@gmail.com>
2022-04-25 01:59:05 +0600

    create order-block

--- pages/main.php
+++ pages/main.php
@@ -23,6 +23,18 @@ $titil = "Home";?>
         </div>
       </div>
     </header>
+    <div class="order">
+      <div class="order-info-block">
+        <div class="order-info-block_content">
+          <h1>Доставка товаров из Великобритании в Россию</h1>
+          <p>Начните экономить до 80% на шопинге. Регистрируйтесь в OkeyPost, чтобы покупать со скидками в UK одежду, обувь, гаджеты известных брендов и безопасно отправлять вещи в Россию.</p>
+          <button>Получить адрес в UK</button>
+        </div>
+        <div class="order-info-block_img">
+          <img src="../assets/img/okeypost/Иллюстрация.png" alt="">
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 
