89be647b76635bdba44272164094f8ccc592710b
unknown <bika070705@gmail.com>
2022-04-27 10:30:11 +0600

    create card-block

--- pages/main.php
+++ pages/main.php
@@ -65,6 +65,40 @@ $titil = "Home";?>
         </div>
       </div>
     </div>
+    <div class="simple contaner">
+      <div class="simple-block">
+        <h1 class="simple-block_title">4 простых шага для ваших покупок в Великобритании </h1>
+        <div class="simple-block_cards">
+          <div class="simple-block_card">
+            <img src="../assets/img/okeypost/simple/1.svg" alt="">
+            <h1>вы ВЫБИРАЕТЕ ТОВАР</h1>
+            <p>в английском магазине, оплачиваете и указываете адрес склада OkeyPost</p>
+          </div>
+          <div class="simple-block_card">
+            <img src="./assets/img/okeypost/simple/2.svg" alt="">
+            <h1>ПОКУПКИ ПРИХОДЯТ НА СКЛАД</h1>
+            <p>и появляются в вашем личном кабинете на нашем сайте</p>
+          </div>
+          <div class="simple-block_card">
+            <img src="./assets/img/okeypost/simple/3.svg" alt="">
+            <h1>ОТПРАВЛЯЕМ ИХ К ВАМ ДОМОЙ</h1>
+            <p>в надежной упаковке, по выгодной цене и очень быстро</p>
+          </div>
+          <div class="simple-block_card">
+            <img src="./assets/img/okeypost/simple/4.svg" alt="">
+            <h1>ЗАБИРАЕТЕ свою ПОСЫЛКУ</h1>
+            <p>в пункте выдачи, на почте или с курьером</p>
+          </div>
+        </div>
+        <div class="">
+          <p class="simple-block_desc">Не хотите разбираться самостоятельно, наши мастера шопинга с радостью купят все товары за вас.</p>
+          <button class="simple-block_btn">Быстрая покупка</button>
+        </div>
+      </div>
+    </div>
+    <div class="">
+
+    </div>
   </div>
 </div>
 
