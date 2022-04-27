f2b6a77e7977a4fe90ebc8ece27e97c4b5d2e5d2
unknown <bika070705@gmail.com>
2022-04-27 13:49:55 +0600

    something

--- pages/main.php
+++ pages/main.php
@@ -99,6 +99,102 @@ $titil = "Home";?>
     <div class="">
 
     </div>
+    <!-- <div class="comment contaner">
+      <h1 class="comment_title">Отзывы и цитаты блогеров:</h1>
+      <div class="comment-cards">
+        <div class="comment-card">
+          <div class="comment-card_img">
+              <img src="./assets/img/okeypost/comment/1.png" alt="">
+            </div>
+          <div class="">
+            <div class="comment-card_top">
+              <h1>NIKOLAY S</h1>
+              <div class="comment-card_star">
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+              </div>
+              <p>Ваша доставка самая быстрая и дешёвая!!!</p>
+            </div>
+            <div class="comment-card_bottom">
+              <h1>Дата:<span>16 Ноября 2021</span></h1>
+              <h1>Город:<span>Санкт-Петербург</span></h1>
+            </div>
+          </div>
+        </div>
+
+        <div class="comment-card">
+          <div class="comment-card_img">
+              <img src="./assets/img/okeypost/comment/2.png" alt="">
+            </div>
+          <div class="">
+            <div class="comment-card_top">
+              <h1>DMITRIY D</h1>
+              <div class="comment-card_star">
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+              </div>
+              <p>Пуховик Levi’s c eBay. Всё приехало быстро и целым.</p>
+            </div>
+            <div class="comment-card_bottom">
+              <h1>Дата:<span>16 Ноября 2021</span></h1>
+              <h1>Город:<span> Москва</span></h1>
+            </div>
+          </div>
+        </div>
+
+        <div class="comment-card">
+          <div class="comment-card_img">
+              <img src="./assets/img/okeypost/comment/3.png" alt="">
+            </div>
+          <div class="">
+            <div class="comment-card_top">
+              <h1>VLADIMIR E</h1>
+              <div class="comment-card_star">
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+              </div>
+              <p>Огонь, как всегда! Отдельное спасибо за доп.упаковку, часики от Apple пришли в простом бумажном пакете <p>
+            </div>
+            <div class="comment-card_bottom">
+              <h1>Дата:<span>16 Ноября 2021</span></h1>
+              <h1>Город:<span>Санкт-Петербург</span></h1>
+            </div>
+          </div>
+        </div>
+
+        <div class="comment-card">
+          <div class="comment-card_img">
+              <img src="./assets/img/okeypost/comment/4.png" alt="">
+            </div>
+          <div class="">
+            <div class="comment-card_top">
+              <h1>Оксана</h1>
+              <div class="comment-card_star">
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+              </div>
+              <p>Очень быстро, надежно! Как всегда большое спасибо за работу!</p>
+            </div>
+            <div class="comment-card_bottom">
+              <h1>Дата:<span>16 Ноября 2021</span></h1>
+              <h1>Город:<span>Odintsovo</span></h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> -->
   </div>
 </div>
 
